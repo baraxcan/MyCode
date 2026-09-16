cc41341ac089bfd02a374112d66b76450ec25f44
baraxcan <baraxcankarataiev@gmail.com>
2026-09-16 19:50:53 +0900

    NEW UPDATE

---
+++ math/sieve_of_Eratosthenes.cpp
@@ -0,0 +1,12 @@
+// time   - O( n * loglog(n) )
+// memory - O(n)
+int n; cin >> n;
+vector<bool> is_prime(n + 1, true);
+is_prime[0] = isprime[1] = false;
+for (int i = 2; i * i <= n; ++i) {
+    if (is_prime[i]) {
+        for )int j = i * i; j <= n; j += i) {
+            is_prime[j] = false;
+        }
+    }
+}
